c72e55979e04e141cbcd15590deccdab84026bf4
Maninder Singh <manindersingh@Maninders-MBP.ht.home>
2023-02-04 19:03:54 -0500

    added the kafka config class and bean

---
+++ src/main/java/com/optum/labs/kafka/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package com.optum.labs.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic newTopic() {
+        return TopicBuilder.name("optum-labs-topic")
+//                .partitions(3): we can pass partitions like this but in this example,
+//                //we will go with default option
+                .build();
+    }
+}
