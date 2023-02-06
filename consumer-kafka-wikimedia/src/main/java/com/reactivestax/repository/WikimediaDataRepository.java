1ea41581654a7f4c216a16725af639d0ddf33610
Maninder Singh <manindersingh@Maninders-MBP.localdomain>
2023-02-06 10:04:53 -0500

    added database support to save data

---
+++ consumer-kafka-wikimedia/src/main/java/com/reactivestax/repository/WikimediaDataRepository.java
@@ -0,0 +1,11 @@
+package com.reactivestax.repository;
+
+import com.reactivestax.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData,Long> {
+
+
+}
--- consumer-kafka-wikimedia/src/main/java/com/reactivestax/service/KafkaDatabaseConsumer.java
+++ consumer-kafka-wikimedia/src/main/java/com/reactivestax/service/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package com.reactivestax.service;
 
+import com.reactivestax.entity.WikimediaData;
+import com.reactivestax.repository.WikimediaDataRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -8,10 +10,18 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaDatabaseConsumer {
 
+    private WikimediaDataRepository wikimediaDataRepository;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository wikimediaDataRepository) {
+        this.wikimediaDataRepository = wikimediaDataRepository;
+    }
+
     @KafkaListener(topics = "${spring.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(String eventMessage){
         log.info("Message received :{}",eventMessage);
-
+        WikimediaData wikimediaData= new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+        wikimediaDataRepository.save(wikimediaData);
     }
 
 
